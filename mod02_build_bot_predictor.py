ca06e63a6d6a4e80040716104ab66305ee244fb2
JNUSO <Jcnuso@protonmail.com>
2026-09-17 18:34:16 -0400

    added test with very low max depth

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -13,7 +13,7 @@ def train_model(X, y, seed=seed):
         learning_rate=0.005,
         n_estimators=1000,
         max_depth=5,
-        subsample=2,
+        subsample=1,
         min_samples_leaf=1,
         random_state=seed
     )
